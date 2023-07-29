11f2b090d2c38819c767d8af35f0305b8f379c38
Masaki-ohara <masakingu921@gmail.com>
2023-07-29 18:43:15 +0900

    javascript練習

---
+++ 02-10_variables-and-constants /main.js
@@ -0,0 +1,22 @@
+'use strict';
+let color = 'Red';
+console.log(color);
+
+color = 'Green';
+console.log(color);
+
+// let color = 'Blue';
+
+let myColor = 'Blue';
+console.log(myColor);
+
+var myName = 'Nakamura';
+console.log(myName);
+
+// 定数
+const PI = 3.14;
+console.log(PI);
+// PI = 3.1; NG
+
+const TAX_RATE = 10;
+console.log(TAX_RATE);
---
+++ 23_operator/main.js
@@ -0,0 +1,4 @@
+let mass = 54
+let height = 1.6
+let BMI = mass / (height * height);
+console.log(BMI);
